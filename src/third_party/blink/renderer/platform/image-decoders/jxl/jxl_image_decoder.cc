cc481a5cd7e349a48d6d3de0494a6e893fe6bd26
Alexander Frick <Alex313031@gmail.com>
2025-09-29 15:58:25 -0500

    corrections to allow compile in M138

--- src/third_party/blink/renderer/platform/image-decoders/jxl/jxl_image_decoder.cc
+++ src/third_party/blink/renderer/platform/image-decoders/jxl/jxl_image_decoder.cc
@@ -8,6 +8,11 @@
 #include "third_party/blink/renderer/platform/image-decoders/fast_shared_buffer_reader.h"
 #include "third_party/skia/include/core/SkColorSpace.h"
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 namespace blink {
 
 namespace {
