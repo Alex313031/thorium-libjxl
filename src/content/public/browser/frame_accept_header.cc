cac66bf5c2c3cdbcc70435c6f14bb9596b4a28d7
Alexander Frick <Alex313031@gmail.com>
2026-02-19 12:53:15 -0600

    fix missing 2026 copyright update

--- src/content/public/browser/frame_accept_header.cc
+++ src/content/public/browser/frame_accept_header.cc
@@ -1,4 +1,4 @@
-// Copyright 2024 The Chromium Authors, Alex313031, and win32ss
+// Copyright 2026 The Chromium Authors, Alex313031, and win32ss
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
